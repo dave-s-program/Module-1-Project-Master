860afb90dc15ca343d50476c6b990cdaa8a6654b
Dave Flores Malhin <suppordev199x@gmail.com>
2022-12-05 18:59:49 +0800

    Module 1 Project Master

---
+++ main.test.js
@@ -0,0 +1,7 @@
+// import { newsList } from "./main.js";
+import { sort, search, newsList } from "./main.js";
+
+//Example testing for search function
+test("Search function testing", () => {
+  expect(search(newsList)).toBe("I am a cool web developer");
+});
